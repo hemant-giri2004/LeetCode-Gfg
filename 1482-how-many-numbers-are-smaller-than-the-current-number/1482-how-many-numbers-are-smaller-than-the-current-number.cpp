d45634a9092271d15a9f16188c823b2bf9b718e1
Hemant Giri <122727048+hemant-giri2004@users.noreply.github.com>
2024-07-09 00:20:38 +0530

    Time: 0 ms (100%), Space: 12.8 MB (23.39%) - LeetHub

--- 1482-how-many-numbers-are-smaller-than-the-current-number/1482-how-many-numbers-are-smaller-than-the-current-number.cpp
+++ 1482-how-many-numbers-are-smaller-than-the-current-number/1482-how-many-numbers-are-smaller-than-the-current-number.cpp
@@ -3,10 +3,13 @@ public:
     vector<int> smallerNumbersThanCurrent(vector<int>& nums) {
         vector<int >ans;
         int count[101]={0};
+        //count the frequency of elements.
         for(auto n:nums)
             count[n]++;
+        //calculate cumulative frequency.
         for(int i=1;i<101;i++)
             count[i]+=count[i-1];
+        //return the previous cummulative frequency.
         for(int i=0;i<nums.size();i++)
             if(nums[i]!=0)
                 ans.push_back(count[nums[i]-1]);
