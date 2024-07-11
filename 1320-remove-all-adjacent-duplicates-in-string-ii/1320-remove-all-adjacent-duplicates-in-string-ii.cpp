852ef0c307d83cfcd0215662de05734805b37544
Hemant Giri <122727048+hemant-giri2004@users.noreply.github.com>
2024-07-12 03:00:40 +0530

    Time: 12 ms (88.14%), Space: 12.4 MB (52.97%) - LeetHub

--- 1320-remove-all-adjacent-duplicates-in-string-ii/1320-remove-all-adjacent-duplicates-in-string-ii.cpp
+++ 1320-remove-all-adjacent-duplicates-in-string-ii/1320-remove-all-adjacent-duplicates-in-string-ii.cpp
@@ -8,19 +8,34 @@ public:
         }
         return true;
     }
-    string removeDuplicates(string s, int k) {
-        string ans="";
-        for(auto ch:s){
-            if(ans.size()<k-1)
-                ans.push_back(ch);
-            else{
-                if(lastKCharIsEqual(ans,ch,k-1))
-                    for(int i=0;i<k-1;i++)
-                        ans.pop_back();
-                else
-                    ans.push_back(ch);
-            }
+    string twoPointerApproch(string &s,int &k){
+        vector<int >count(s.size());
+        int i=0,j=0;
+        while(j<s.size()){
+            s[i]=s[j];
+            count[i]=1;
+            if(i>0 && s[i]==s[i-1])
+                count[i]+=count[i-1];
+            if(count[i]==k)
+                i-=k;
+            i++,j++;
         }
-        return ans;
+        return s.substr(0,i);
+    }
+    string removeDuplicates(string s, int k) {
+        // string ans="";
+        // for(auto ch:s){
+        //     if(ans.size()<k-1)
+        //         ans.push_back(ch);
+        //     else{
+        //         if(lastKCharIsEqual(ans,ch,k-1))
+        //             for(int i=0;i<k-1;i++)
+        //                 ans.pop_back();
+        //         else
+        //             ans.push_back(ch);
+        //     }
+        // }
+        // return ans;
+        return twoPointerApproch(s,k);
     }
 };
