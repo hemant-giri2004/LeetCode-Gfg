103a6a1fcbb3f0ad23319c59a0787048fe1dbc35
Hemant Giri <122727048+hemant-giri2004@users.noreply.github.com>
2024-07-13 00:36:40 +0530

    Time: 25 ms (24.75%), Space: 27.5 MB (40.19%) - LeetHub

---
+++ 0189-rotate-array/0189-rotate-array.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    void rotate(vector<int>& nums, int k) {
+        //this give tle.
+        // while(k--){
+        //     int last=nums[nums.size()-1];
+        //     for(int i=nums.size()-1;i>0;i--){
+        //         nums[i]=nums[i-1];
+        //     }
+        //     nums[0]=last;
+        // }
+        int n=nums.size();
+        k%=n;
+        int i=0,j=n-k-1;
+        while(i<j && j<nums.size()){
+            swap(nums[i],nums[j]);
+            i++,j--;
+        }
+        i=n-k,j=nums.size()-1;
+                while(i<j){
+            swap(nums[i],nums[j]);
+            i++,j--;
+        }
+        i=0,j=nums.size()-1;
+        while(i<j){
+            swap(nums[i],nums[j]);
+            i++,j--;
+        }
+
+    }
+};
