8c2f7d73e1e406c7605f6e8fb59e9ce9687f1e1a
Hemant Giri <122727048+hemant-giri2004@users.noreply.github.com>
2024-07-22 21:58:15 +0530

    Time: 0 ms (100%), Space: 9.6 MB (5.89%) - LeetHub

---
+++ 0027-remove-element/0027-remove-element.c
@@ -0,0 +1,11 @@
+int removeElement(int* nums, int numsSize, int val) {
+    int k=0,i=0;
+    while(i<numsSize){
+        if(nums[i]!=val){
+            nums[k]=nums[i];
+            k++;
+        }
+        i++;
+    }
+    return k;
+}
