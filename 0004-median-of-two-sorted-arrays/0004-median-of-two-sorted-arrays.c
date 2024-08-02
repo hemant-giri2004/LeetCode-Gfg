1c7d72760c2b36a2ac673ada0f2e35999be45b6d
Hemant Giri <122727048+hemant-giri2004@users.noreply.github.com>
2024-08-02 22:24:19 +0530

    Time: 7 ms (91.87%), Space: 11.2 MB (20.93%) - LeetHub

---
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.c
@@ -0,0 +1,19 @@
+double findMedianSortedArrays(int* nums1, int nums1Size, int* nums2, int nums2Size)
+{
+    int sort[nums1Size+nums2Size];
+    int i=0,j=0,k=0;
+    while (j < nums1Size && k < nums2Size){
+        if (nums1[j] < nums2[k])
+            sort[i++] = nums1[j++];
+        else
+            sort[i++] = nums2[k++];
+    }
+    while (j < nums1Size) 
+        sort[i++] = nums1[j++];
+    while (k < nums2Size)
+        sort[i++] = nums2[k++];
+
+    int mid=i/2;
+    if(i%2==0)  return (double)(sort[mid]+sort[mid-1])/2;
+    else    return (double)sort[mid];   
+}
