a4a409dfcaeb89dd965950fc6c12a53831b1c53a
Hemant Giri <122727048+hemant-giri2004@users.noreply.github.com>
2024-08-04 21:29:17 +0530

    Time: 0 ms (100%), Space: 7.7 MB (38.63%) - LeetHub

---
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.c
@@ -0,0 +1,14 @@
+int strStr(char* haystack, char* needle) {
+    int i = 0, j = 0;
+    while (haystack[i] != '\0') {
+        while (haystack[i+j] == needle[j] && needle[j] != '\0') {
+            j++;
+        }
+        if (needle[j] == '\0') {
+            return i;
+        }
+        i++;
+        j = 0;
+    }
+    return -1;
+}
