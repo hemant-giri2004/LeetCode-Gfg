e439565307c1f1fc8ec7113aafe45c7eda6ac3fc
Hemant Giri <122727048+hemant-giri2004@users.noreply.github.com>
2024-08-10 22:24:11 +0530

    Time: 3 ms (49.54%), Space: 8.5 MB (30.46%) - LeetHub

---
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.c
@@ -0,0 +1,18 @@
+int myAtoi(char* s) {
+    long num = 0;
+    int sign = 1, b = 1, i = 0;
+    while (s[i] == ' ')
+        i++;
+    if(s[i]=='+'||s[i]=='-'){
+        if(s[i]=='-')
+            sign=-1;
+        i++;
+    }
+    while(s[i] >= '0' && s[i] <= '9'){
+        num = num * 10 + (int)(s[i] - '0');
+        if (num > INT_MAX) 
+             return (sign == 1) ? INT_MAX : INT_MIN;
+        i++;
+    }
+        return (int)(sign*num);
+}
