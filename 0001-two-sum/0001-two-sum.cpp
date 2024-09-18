2739be28baa9d0cf0edf2b2b8db1ac4c629bbb13
Hemant Giri <122727048+hemant-giri2004@users.noreply.github.com>
2024-09-18 10:23:04 +0530

    Time: 10 ms (64.97%), Space: 14.6 MB (18.38%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        map<int,int> mp;
+        int n=nums.size();
+        for(int i=0;i<n;i++){
+            int num=nums[i];
+            int more=target-num;
+            //because map return the index if element is find else return the index.
+            if(mp.find(more)!=mp.end()){
+                return {mp[more],i};
+            }
+            mp[num]=i;
+        }
+        return {-1,-1};
+    }
+};
