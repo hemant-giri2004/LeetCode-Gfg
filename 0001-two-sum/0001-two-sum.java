072e79cd3879b8222cafbe12a80ac3c01a8c49f9
Hemant Giri <122727048+hemant-giri2004@users.noreply.github.com>
2024-11-03 00:00:00 +0530

    Update 0001-two-sum.java

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -3,6 +3,7 @@ import java.util.Map;
 
 public class Solution {
     public int[] twoSum(int[] nums, int target) {
+        
         Map<Integer, Integer> map = new HashMap<>();
 
         for (int i = 0; i < nums.length; i++) {
