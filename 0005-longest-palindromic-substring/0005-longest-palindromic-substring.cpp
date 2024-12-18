36911915afc54bb858e082c323d0eac8a02e5c1c
Hemant Giri <122727048+hemant-giri2004@users.noreply.github.com>
2024-12-18 23:50:31 +0530

    Time: 2189 ms (5%), Space: 491.7 MB (5.01%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
@@ -1,17 +1,14 @@
 class Solution {
 public:
     bool isPalindrome(string &s,int i, int j){
-
-        
         while(i<=j){
             if(s[i]!=s[j])
                 return false;
-            
             i++,j--;
         }
         return true;
     }
-    string longestPalindrome(string s) {   
+    string longestPalindrome(string s) {
         string ans;
         for(int i=0;i<s.size();i++){
             for(int j=i;j<s.size();j++){
