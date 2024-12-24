dd3453a0953ee69cb22ab59151912617b2acfc1a
Hemant Giri <122727048+hemant-giri2004@users.noreply.github.com>
2024-12-24 23:29:58 +0530

    Time: 3 ms (77.31%), Space: 15.1 MB (12.62%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -2,6 +2,7 @@ class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
         map<int,int> mp;
+        
         int n=nums.size();
         for(int i=0;i<n;i++){
             int num=nums[i];
