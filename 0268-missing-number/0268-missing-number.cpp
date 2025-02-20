5e4c5120e2d527e5edc3158d5c33ea6ef25698f4
Hemant Giri <122727048+hemant-giri2004@users.noreply.github.com>
2025-02-21 02:48:37 +0530

    Time: 1 ms (25.84%), Space: 21.8 MB (70.73%) - LeetHub

---
+++ 0268-missing-number/0268-missing-number.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        int n=nums.size();
+        // //brute force
+        // for(int i=0;i<n;i++){
+        //     int flag=0;
+        //     for(int j=0;j<n;j++){
+        //         if(nums[j]==i){
+        //             flag=1;
+        //             break;
+        //         }
+        //     }
+        //     if(flag==0) return i;
+        // }
+
+        // // nlogn approach using sorting.
+        // sort(nums.begin(),nums.end());
+        // for(int i=0;i<n;i++){
+        //     if(nums[i]!=i)  return i;
+        // }
+        // return n;
+
+        //using maths.
+        long s1=0;
+        for(int i=0;i<n;i++)
+            s1+=nums[i];    //find the sum of all element.
+        long s2=n*(n+1)/2;  //find the sum of n consucetive no.
+        return s2-s1;       //return difference.
+    }
+};
