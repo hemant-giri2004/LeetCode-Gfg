61298e7bb001b7cbc5292b56be3b7ae0a781d365
Hemant Giri <122727048+hemant-giri2004@users.noreply.github.com>
2025-05-09 02:47:53 +0530

    Time: 0 ms (100%), Space: 28.2 MB (58.58%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -13,11 +13,11 @@ public:
         //         return n;
         // }
 
-        // nlogn time 
-        sort(nums.begin(),nums.end());
-        return nums[nums.size()/2];
+        // // nlogn time 
+        // sort(nums.begin(),nums.end());
+        // return nums[nums.size()/2];
         
-
+        // // n time
         // unordered_map <int,int> countMap;
         // int majorityCount=nums.size()/2;
         // for(auto num:nums){
@@ -25,6 +25,21 @@ public:
         //     if(countMap[num]>majorityCount)
         //         return num;
         // }
+
+        int cnt=1;
+        int ele=nums[0];
+        for(int i=1;i<nums.size();i++){
+            if(cnt==0){
+                cnt++;
+                ele=nums[i];
+            }
+            else if(nums[i]==ele){
+                cnt++;
+            } 
+            else
+                cnt--;
+        }
+        return ele;
         return -1;
     }
 };
