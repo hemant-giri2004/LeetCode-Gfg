b6d43362c5c8b72e556416fe8b7002c8b248996d
Hemant Giri <122727048+hemant-giri2004@users.noreply.github.com>
2025-05-09 02:42:48 +0530

    Time: 8 ms (8.09%), Space: 28.1 MB (84.12%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -1,13 +1,30 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-        unordered_map <int,int> countMap;
-        int majorityCount=nums.size()/2;
-        for(auto num:nums){
-            countMap[num]++;
-            if(countMap[num]>majorityCount)
-                return num;
-        }
+
+        // //n^2 time
+        // for(auto n: nums){
+        //     int cnt=0;
+        //     for(auto m:nums){
+        //         if(n==m)
+        //             cnt++;
+        //     }
+        //     if(cnt>(nums.size()/2))
+        //         return n;
+        // }
+
+        // nlogn time 
+        sort(nums.begin(),nums.end());
+        return nums[nums.size()/2];
+        
+
+        // unordered_map <int,int> countMap;
+        // int majorityCount=nums.size()/2;
+        // for(auto num:nums){
+        //     countMap[num]++;
+        //     if(countMap[num]>majorityCount)
+        //         return num;
+        // }
         return -1;
     }
 };
