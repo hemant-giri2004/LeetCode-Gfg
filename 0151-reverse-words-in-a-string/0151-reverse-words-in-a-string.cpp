1305abee01793bc391c741432643b6a41c940e3e
Hemant Giri <122727048+hemant-giri2004@users.noreply.github.com>
2025-05-11 00:26:31 +0530

    Time: 3 ms (41.89%), Space: 10.2 MB (71.89%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
@@ -1,19 +1,38 @@
 class Solution {
 public:
     string reverseWords(string s) {
-        int  n=s.length();
-        string str="";
-        //reverse sentence
+        
+        // int  n=s.length();
+        // string str="";
+        // //reverse sentence
+        // reverse(s.begin(),s.end());
+        // for(int i=0;i<n;i++){
+        //     string word="";
+        //     while(i<n && s[i]!=' ')
+        //         word+=s[i++];
+        //     //reverse word
+        //     reverse(word.begin(),word.end());
+        //     if(word.length()>0)
+        //         str+=" "+word;
+        // }
+        // return str.substr(1);
+
+        int n=s.size();
+        //reverse whole string
         reverse(s.begin(),s.end());
-        for(int i=0;i<n;i++){
-            string word="";
-            while(i<n && s[i]!=' ')
-                word+=s[i++];
-            //reverse word
-            reverse(word.begin(),word.end());
-            if(word.length()>0)
-                str+=" "+word;
+        int i=0,l=0,r=0;
+        //travere whole string and reverse character between l or r.
+        while(i<n){
+            while(i<n  && s[i]!=' ')
+                s[r++]=s[i++];
+            if(l<r){
+                reverse(s.begin()+l,s.begin()+r);
+                s[r]=' ';
+                r++;
+                l=r;
+            }
+            i++;
         }
-        return str.substr(1);
+        return s.substr(0,r-1);
     }
 };
