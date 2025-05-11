fc402b665d64379559aad0518d7875bc2c75e2fa
Hemant Giri <122727048+hemant-giri2004@users.noreply.github.com>
2025-05-11 19:15:13 +0530

    Time: 21 ms (37.68%), Space: 27.1 MB (24.46%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.cpp
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -1,18 +1,41 @@
 class Solution {
 public:
-
+    string findKey(string str){
+        string ans;
+        int arr[26]={0};
+        for(auto ch:str){
+            arr[ch-'a']++;
+        }
+        for(int i=0;i<26;i++){
+            if(arr[i]>0)
+                ans+=string(arr[i],i+'a');
+        }
+        return ans;
+    }
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        // vector<vector<string>> ans;
+        // unordered_map<string,vector<string> > mp;
+        // for(auto str:strs){
+        //     string temp=str;
+        //     sort(temp.begin(),temp.end());
+        //     mp[temp].push_back(str);
+        // } 
+        // for(auto str:mp){
+        //     ans.push_back(str.second);
+        // }
+        // return ans;
+
         vector<vector<string>> ans;
         unordered_map<string,vector<string> > mp;
         for(auto str:strs){
-            string temp=str;
-            sort(temp.begin(),temp.end());
-            mp[temp].push_back(str);
+            string key=findKey(str);
+            mp[key].push_back(str);
         } 
         for(auto str:mp){
-            vector<string> temp=str.second;
-            ans.push_back(temp);
+            ans.push_back(str.second);
         }
         return ans;
+
+        
     }
 };
