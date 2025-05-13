9c7a8bdca679b129ea5b15340e2109551958c157
Hemant Giri <122727048+hemant-giri2004@users.noreply.github.com>
2025-05-13 23:48:17 +0530

    Update 0053-maximum-subarray.c

--- 0053-maximum-subarray/0053-maximum-subarray.c
+++ 0053-maximum-subarray/0053-maximum-subarray.c
@@ -2,6 +2,7 @@ int maxSubArray(int* nums, int numsSize) {
     int sum=0;
     int maxSum=nums[0];
     for(int i=0;i<numsSize;i++){
+        
         sum=sum+nums[i];
         maxSum=(maxSum>sum)?maxSum:sum;
         if(sum<0)
