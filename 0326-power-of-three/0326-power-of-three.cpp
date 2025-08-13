aa7033cacaaf87e84e53db2936853653a2d0f9b0
Hemant Giri <122727048+hemant-giri2004@users.noreply.github.com>
2025-08-13 23:32:37 +0530

    Time: 3 ms (60.14%), Space: 8.7 MB (99.9%) - LeetHub

--- 0326-power-of-three/0326-power-of-three.cpp
+++ 0326-power-of-three/0326-power-of-three.cpp
@@ -1,12 +1,15 @@
 class Solution {
 public:
     bool isPowerOfThree(int n) {
-        if(n==0)    
-            return false;
-        while(n%3==0)   
-            n/=3;
-        if(n==1)
-            return true;
-        return false;
+        // //logn time complexicity where log base is 3.
+        // if(n<=0)    
+        //     return false;
+        // while(n%3==0)   
+        //     n/=3;
+        // if(n==1)
+        //     return true;
+        // return false;
+        int maxPow=pow(3,19);
+        return (n>0 && maxPow%n==0);
     }
 };
