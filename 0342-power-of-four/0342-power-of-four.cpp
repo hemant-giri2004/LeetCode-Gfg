a45969b8538c488e63678b10f2e775b816f2028c
Hemant Giri <122727048+hemant-giri2004@users.noreply.github.com>
2025-08-15 23:04:06 +0530

    Time: 0 ms (100%), Space: 8.1 MB (4.79%) - LeetHub

--- 0342-power-of-four/0342-power-of-four.cpp
+++ 0342-power-of-four/0342-power-of-four.cpp
@@ -1,10 +1,17 @@
 class Solution {
 public:
     bool isPowerOfFour(int n) {
-        if(n==0)    return false;
-        while(n%4==0)
-            n/=4;
-        if(n==1)
+        // if(n==0)    return false;
+        // while(n%4==0)
+        //     n/=4;
+        // if(n==1)
+        //     return true;
+      
+        // return false;
+
+        if(n<=0)    return 0;
+        int x=log(n)/log(4);
+        if(pow(4,x)==n)
             return true;
         return false;
     }
