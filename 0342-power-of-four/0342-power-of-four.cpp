1c8b59aee670691c22c8cab22dfac4616d7317dd
Hemant Giri <122727048+hemant-giri2004@users.noreply.github.com>
2025-08-15 23:06:11 +0530

    Time: 0 ms (100%), Space: 7.9 MB (18.14%) - LeetHub

--- 0342-power-of-four/0342-power-of-four.cpp
+++ 0342-power-of-four/0342-power-of-four.cpp
@@ -9,9 +9,14 @@ public:
       
         // return false;
 
-        if(n<=0)    return 0;
-        int x=log(n)/log(4);
-        if(pow(4,x)==n)
+        // if(n<=0)    return 0;
+        // int x=log(n)/log(4);
+        // if(pow(4,x)==n)
+        //     return true;
+        // return false;
+
+        if(n<=0)    return false;
+        if((n&(n-1))==0 && (n-1)%3==0)
             return true;
         return false;
     }
