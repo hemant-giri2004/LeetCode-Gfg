02165ca996f50bcd2c9323f9149d4a4b56ca5859
Hemant Giri <122727048+hemant-giri2004@users.noreply.github.com>
2025-08-18 23:47:52 +0530

    Time: 64 ms (18.5%), Space: 30.6 MB (18.28%) - LeetHub

---
+++ 0679-24-game/0679-24-game.cpp
@@ -0,0 +1,48 @@
+class Solution {
+public:
+    double epsilon=0.1;
+    bool solve(vector<double>& cards){
+        if(cards.size()== 1)
+            return abs(cards[0]-24) <= epsilon;
+        
+        //pick two possible numbers
+        for(int i=0; i<cards.size() ; i++){
+            for(int j=0; j<cards.size() ; j++){
+                if(i==j)
+                    continue;
+                
+                vector<double> temp;
+                for(int k=0; k < cards.size(); k++)
+                    if(k != i && k != j)
+                        temp.push_back(cards[k]);
+
+                double a=cards[i];
+                double b=cards[j];
+            
+                vector<double> possiVal={a+b,a-b,b-a,a*b};
+                if(abs(b)> 0.0){
+                    possiVal.push_back(a/b);
+                }
+
+                if(abs(a)>0.0){
+                    possiVal.push_back(b/a);
+                }
+                for(double val : possiVal){
+                    temp.push_back(val);    //DO
+                    if(solve(temp)==true)   //EXPLORE
+                        return true;
+                    temp.pop_back();        //UNDO  
+                }
+            }
+        }
+        return false;
+    }
+    bool judgePoint24(vector<int>& cards) {
+        vector<double> nums;
+        for(int i=0; i<cards.size();i++){
+            nums.push_back(1.0*cards[i]);
+        }
+
+        return solve(nums);
+    }
+};
