669ef0e3c7be4a18735c040c6be9fdb36ae1b18f
Hemant Giri <122727048+hemant-giri2004@users.noreply.github.com>
2025-08-19 23:45:38 +0530

    Time: 3 ms (40.28%), Space: 111.2 MB (95.09%) - LeetHub

--- 2432-number-of-zero-filled-subarrays/2432-number-of-zero-filled-subarrays.cpp
+++ 2432-number-of-zero-filled-subarrays/2432-number-of-zero-filled-subarrays.cpp
@@ -3,19 +3,30 @@ public:
     long long zeroFilledSubarray(vector<int>& nums) {
         int n=nums.size();
         long long totalSubArr=0;
-        int i=0;
-        while(i<n){
-            long long len=0;
+        // int i=0;
+        // while(i<n){
+        //     long long len=0;
+        //     if(nums[i]==0){
+        //         while(i<n && nums[i]==0){
+        //             len++;
+        //             i++;
+        //         }
+        //     }
+        //     else{
+        //         i++;
+        //     }
+        //     totalSubArr+=(len*(len+1)/2);
+        // }
+
+        //approach 2
+        int count=0;
+        for(int i=0;i<n; i++){
             if(nums[i]==0){
-                while(i<n && nums[i]==0){
-                    len++;
-                    i++;
-                }
+                count++;
+            }else{
+                count=0;
             }
-            else{
-                i++;
-            }
-            totalSubArr+=(len*(len+1)/2);
+            totalSubArr+=count;
         }
         return totalSubArr;
     }
