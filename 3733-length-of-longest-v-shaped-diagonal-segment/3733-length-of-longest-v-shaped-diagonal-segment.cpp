0e06b4a3d2495c706fe00a8f12b8627a047a843f
Hemant Giri <122727048+hemant-giri2004@users.noreply.github.com>
2025-08-27 23:47:03 +0530

    Time: 253 ms (84.49%), Space: 103.8 MB (71.66%) - LeetHub

--- 3733-length-of-longest-v-shaped-diagonal-segment/3733-length-of-longest-v-shaped-diagonal-segment.cpp
+++ 3733-length-of-longest-v-shaped-diagonal-segment/3733-length-of-longest-v-shaped-diagonal-segment.cpp
@@ -2,12 +2,16 @@ class Solution {
 public:
     vector<vector<int>> direction = {{1, 1}, {1, -1}, {-1, -1}, {-1, 1}};
     int m,n;
+    int dp[501][501][4][2];
     int solve(int i, int j, int d, bool canTurn, int val,vector<vector<int>>& grid){
         int i_=i+direction[d][0];
         int j_=j+direction[d][1];
 
         if(i_<0 || i_>=m ||j_<0 ||j_>=n || grid[i_][j_]!=val){
             return 0;
+        }
+        if(dp[i_][j_][d][canTurn]!=-1){
+            return dp[i_][j_][d][canTurn];
         }
         int result=0;
         int keepMoving=1+solve(i_,j_,d,canTurn,val==2?0:2,grid);
@@ -18,13 +22,13 @@ public:
             result=max(result,turnAndMove);
         }
 
-        return result;
+        return dp[i_][j_][d][canTurn]=result;
     }
 
     int lenOfVDiagonal(vector<vector<int>>& grid) {
         m = grid.size();
         n = grid[0].size();
-
+        memset(dp,-1,sizeof(dp));
         int result = 0;
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
