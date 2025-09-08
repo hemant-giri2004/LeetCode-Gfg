81c6580fd1b583e86f51dfbd89e0bedf48ebd35f
Hemant Giri <122727048+hemant-giri2004@users.noreply.github.com>
2025-09-08 19:43:09 +0530

    Time: 0 ms (100%), Space: 8.3 MB (30.14%) - LeetHub

--- 1440-convert-integer-to-the-sum-of-two-no-zero-integers/1440-convert-integer-to-the-sum-of-two-no-zero-integers.cpp
+++ 1440-convert-integer-to-the-sum-of-two-no-zero-integers/1440-convert-integer-to-the-sum-of-two-no-zero-integers.cpp
@@ -10,13 +10,24 @@ public:
         return true;
     }
     vector<int> getNoZeroIntegers(int n) {
-        //n = a + b where b=n-a
-        for(int a=1;a<n;a++){
-            int b=n-a;
-            if(notContainZero(a) && notContainZero(b)){
-                return {a,b};
-            }
+        // //n = a + b where b=n-a
+        // for(int a=1;a<n;a++){
+        //     int b=n-a;
+        //     if(notContainZero(a) && notContainZero(b)){
+        //         return {a,b};
+        //     }
+        // }
+        // return {-1};
+        int a=n,b=0,placeVal=1;
+        while(n>1){
+            int take=1;
+            if(n%10==1)
+                take=2;
+            a-=take*placeVal;
+            b+=take*placeVal;
+            placeVal*=10;
+            n=(n-take)/10;
         }
-        return {-1};
+        return {a,b};
     }
 };
