60ec8a6418a82414cb756a33dfa3417e76858724
Hemant Giri <122727048+hemant-giri2004@users.noreply.github.com>
2025-09-09 19:49:17 +0530

    Time: 18 ms (17.82%), Space: 9.3 MB (84.16%) - LeetHub

---
+++ 2408-number-of-people-aware-of-a-secret/2408-number-of-people-aware-of-a-secret.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    int M=1e9+7;
+    vector<int> t;
+
+    int solve(int day,int delay,int forget){
+        if(day==1){
+            return 1;
+        }
+        if(day<=0)
+            return 0;
+        if(t[day]!=-1)
+            return t[day];
+        long long result=0;
+        for(int prev=day-forget+1;prev<=day-delay;prev++){
+            if(prev>0){
+                result=(result+solve(prev,delay,forget))%M;
+            }
+        }
+        return t[day]=(int)result;
+    }
+    int peopleAwareOfSecret(int n, int delay, int forget) {
+        int total=0;
+        t.assign(n+1,-1);
+        for(int day=n-forget+1;day <=n; day++){
+            if(day>0){
+                total=(total + solve(day,delay,forget)) % M;
+            }
+        }
+        return total;
+    }
+};
