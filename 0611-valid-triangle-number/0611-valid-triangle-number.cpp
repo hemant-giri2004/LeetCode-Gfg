5a996177985fe21ddd6215960641a6af91c5e2e3
Hemant Giri <122727048+hemant-giri2004@users.noreply.github.com>
2025-09-26 23:54:55 +0530

    Time: 29 ms (77.19%), Space: 16.7 MB (8.77%) - LeetHub

--- 0611-valid-triangle-number/0611-valid-triangle-number.cpp
+++ 0611-valid-triangle-number/0611-valid-triangle-number.cpp
@@ -15,23 +15,50 @@ public:
         return k;
     }
     int triangleNumber(vector<int>& nums) {
+        // int n=nums.size();
+
+        // if(n<3){
+        //     return 0;
+        // }
+
+        // sort(begin(nums),end(nums));
+
+        // int count=0;
+        // for(int i=0;i<n;i++){
+        //     if(nums[i]==0)
+        //         continue;
+        //     for(int j=i+1;j<n;j++){
+        //         int sum=nums[i]+nums[j];
+        //         int k=binarySearch(nums,j+1,n-1,sum);
+        //         if(k!=-1){
+        //             count+=(k-j);
+        //         }
+        //     }
+        // }
+        // return count;
+
         int n=nums.size();
 
         if(n<3){
             return 0;
         }
 
-        sort(begin(nums),end(nums));
+        sort(nums.begin(),nums.end());
 
         int count=0;
-        for(int i=0;i<n;i++){
-            if(nums[i]==0)
-                continue;
-            for(int j=i+1;j<n;j++){
-                int sum=nums[i]+nums[j];
-                int k=binarySearch(nums,j+1,n-1,sum);
-                if(k!=-1){
-                    count+=(k-j);
+
+        for(int k=n-1; k>=2;k--){
+            int i=0;
+            int j=k-1;
+            //nums[i]+nums[j] > nums[k]
+            while(i<j){
+                if(nums[i]+nums[j]>nums[k]){
+                    //nums[i]+nums[k]>nums[j] && nums[k]+nums[j]>nums[i]
+                    count+=(j-i);
+                    j--;
+                }
+                else{
+                    i++;
                 }
             }
         }
