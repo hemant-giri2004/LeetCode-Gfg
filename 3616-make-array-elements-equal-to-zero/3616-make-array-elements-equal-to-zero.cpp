5a6fb7c3bf5a45c9900159fe220ce1e1e87d19b5
Hemant Giri <122727048+hemant-giri2004@users.noreply.github.com>
2025-10-28 22:48:15 +0530

    Time: 0 ms (100%), Space: 22.1 MB (67.89%) - LeetHub

--- 3616-make-array-elements-equal-to-zero/3616-make-array-elements-equal-to-zero.cpp
+++ 3616-make-array-elements-equal-to-zero/3616-make-array-elements-equal-to-zero.cpp
@@ -5,7 +5,7 @@ public:
         while(curr>=0 && curr<N){
             if(nums[curr]>0){
                 nums[curr]--;
-                (dir==1)?dir=-1:dir=1;
+                dir*=-1;
             }
             curr+=dir;
         }
@@ -16,14 +16,34 @@ public:
         return 1;
     }
     int countValidSelections(vector<int>& nums) {
-        int cnt=0;
         N=nums.size();
+        // //n*n
+        // int cnt=0;
+        // for(int i=0;i<N;i++){
+        //     if(nums[i]==0){
+        //         cnt+=check(nums,i,1);    //for right dir
+        //         cnt+=check(nums,i,-1);   //for left dir
+        //     }
+        // }
+        // return cnt;
+
+        //n
+        int totalSum=0;
+        int leftSum=0;
         for(int i=0;i<N;i++){
+            totalSum+=nums[i];
+        }
+        int result=0;
+        for(int i=0;i<N;i++){
+            leftSum+=nums[i];
             if(nums[i]==0){
-                cnt+=check(nums,i,1);    //for right dir
-                cnt+=check(nums,i,-1);   //for left dir
+                int rightSum=(totalSum-leftSum);
+                if(leftSum==rightSum)
+                    result+=2;
+                else if(abs(leftSum-rightSum)==1)
+                    result+=1;
             }
         }
-        return cnt;
+        return result;
     }
 };
