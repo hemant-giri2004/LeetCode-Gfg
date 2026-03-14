18fab0d26e4c1d156042badbdbd49986b3189f5e
Hemant Giri <122727048+hemant-giri2004@users.noreply.github.com>
2026-03-15 01:20:35 +0530

    Time: 0 ms (100%), Space: 7.8 MB (96.95%) - LeetHub

--- 1415-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n/1415-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n.cpp
+++ 1415-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n/1415-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n.cpp
@@ -1,8 +1,29 @@
 class Solution {
 public:
-    void solve(int n,string curr,vector<string>& result){
+    // void solve(int n,string curr,vector<string>& result){
+    //     if(curr.length()==n){
+    //         result.push_back(curr);
+    //         return ;
+    //     }
+    //     for(char ch='a';ch<='c';ch++){
+    //         if(!curr.empty() && curr.back()==ch)
+    //             continue;
+            
+    //         //do
+    //         curr.push_back(ch);
+    //         //explore
+    //         solve(n,curr,result);
+    //         //undo
+    //         curr.pop_back();
+
+    //     }
+    // }
+    void solve(int n,string& curr,int& cnt,int k,string& result){
         if(curr.length()==n){
-            result.push_back(curr);
+            cnt++;
+            if(cnt==k){
+                result=curr;
+            }
             return ;
         }
         for(char ch='a';ch<='c';ch++){
@@ -12,7 +33,9 @@ public:
             //do
             curr.push_back(ch);
             //explore
-            solve(n,curr,result);
+            solve(n,curr,cnt,k,result);
+            // If result is found, exit early
+            if (!result.empty()) return;
             //undo
             curr.pop_back();
 
@@ -20,11 +43,16 @@ public:
     }
     string getHappyString(int n, int k) {
         string curr="";
-        vector<string> result;
-        solve(n,curr,result);
-        if(result.size()<k){
-            return "";
-        }
-        return result[k-1];
+        
+        // vector<string> result;
+        // solve(n,curr,result);
+        // if(result.size()<k){
+        //     return "";
+        // }
+        // return result[k-1];
+        string result="";
+        int cnt=0;
+        solve(n,curr,cnt,k, result);
+        return result;
     }
 };
