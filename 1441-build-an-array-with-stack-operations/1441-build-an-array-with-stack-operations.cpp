0fcb2d9b49e88ff191f36b7c2057741398637c25
Hemant Giri <122727048+hemant-giri2004@users.noreply.github.com>
2026-04-09 00:15:37 +0530

    Time: 3 ms (6.95%), Space: 10.7 MB (86.35%) - LeetHub

---
+++ 1441-build-an-array-with-stack-operations/1441-build-an-array-with-stack-operations.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<string> buildArray(vector<int>& target, int n) {
+        //simulation problem - do question says
+        //because target is strictly increasing order.
+        vector<string> ans;
+        int j=0;
+        for(int i=1;i<=n && j!=target.size();i++){
+            if(target[j]==i){ //number is needed: push it.
+                ans.push_back("Push");
+                j++;
+            }else if(target[j]>i){ //number is not needed.
+                ans.push_back("Push");
+                ans.push_back("Pop");
+            }
+        }
+        return ans;
+    }
+};
