b1f794d2b85e42534dfbd38213e1aa3d9ee08859
Hemant Giri <122727048+hemant-giri2004@users.noreply.github.com>
2026-09-21 23:44:34 +0530

    Time: 0 ms (100%), Space: 7.3 MB (5.88%) - LeetHub

--- 0001-two-sum/0001-two-sum.go
+++ 0001-two-sum/0001-two-sum.go
@@ -1,9 +1,19 @@
 func twoSum(nums []int, target int) []int {
-    for i:=0;i<len(nums)-1;i++{
-        for j:=i+1;j<len(nums);j++{
-            if nums[i]+nums[j]==target{
-                return []int{i,j};
-            }
+    // for i:=0;i<len(nums)-1;i++{
+    //     for j:=i+1;j<len(nums);j++{
+    //         if nums[i]+nums[j]==target{
+    //             return []int{i,j};
+    //         }
+    //     }
+    // }
+    // return nil;
+    mp:=make(map[int]int);
+    for i,num:=range nums{
+        mp[num]=i;
+    }
+    for i,num:=range nums{
+        if j,ok:=mp[target-num];ok && i!=j{
+            return []int{i,j}
         }
     }
     return nil;
