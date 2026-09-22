54d2dd45f95758f76d432c2c16172d993920e238
Hemant Giri <122727048+hemant-giri2004@users.noreply.github.com>
2026-09-23 01:19:42 +0530

    Time: 3 ms (25.66%), Space: 8.6 MB (82.34%) - LeetHub

--- 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.go
+++ 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.go
@@ -1,7 +1,41 @@
 func sortedSquares(nums []int) []int {
-    for i,num:=range nums{
-        nums[i]=num*num
+    // for i,num:=range nums{
+    //     nums[i]=num*num
+    // }
+    // sort.Ints(nums)
+    // return nums
+    start:=len(nums)
+    for i:=1;i<len(nums);i++{
+        if nums[i]>=0{
+            start=i;
+            break
+        }
     }
-    sort.Ints(nums)
-    return nums
+    abs :=func (n int)int{
+        if n<0{
+            return -n
+        }
+        return n
+    }
+    var res []int;
+    i:=start-1
+    j:=start
+    for i>=0 && j<len(nums){
+        if abs(nums[i])<=abs(nums[j]) {
+            res=append(res,nums[i]*nums[i])
+            i--
+        }else{
+            res=append(res,nums[j]*nums[j])
+            j++
+        }
+    }
+    for i>=0 {
+        res=append(res,nums[i]*nums[i])
+        i--
+    }
+    for j<len(nums) {
+        res=append(res,nums[j]*nums[j])
+        j++
+    }
+    return res
 }
